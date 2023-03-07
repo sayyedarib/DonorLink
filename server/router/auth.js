1040a7644fe56f30321a265fb933bbfd1b0b91b5
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-03-07 21:34:05 +0530

    created login backend

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,6 +6,7 @@ const signUpForm = require("../models/signUpForm");
 dotenv.config({ path: "./config.env" });
 require("../db/connection");
 
+//SignUp Route
 router.post("/signUp", async (req, res) => {
   const { name, email, password, cpassword } = req.body;
   if (!name || !email || !password || !cpassword) {
@@ -20,16 +21,44 @@ router.post("/signUp", async (req, res) => {
       return res
         .status(422)
         .json({ message: "password and confirm password mismatched" });
-    }
-    else{
-        const data = new signUpForm({
-            name, email, password, cpassword
-        })
-        await data.save();
-        res.status(202).json({message:"data sent successfully"})
+    } else {
+      const data = new signUpForm({
+        name,
+        email,
+        password,
+        cpassword,
+      });
+      await data.save();
+      res.status(202).json({ message: "data sent successfully" });
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
+  }
+});
+
+
+//login Route
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ error: "email or password not filled " });
+    }
+
+    const userExists = await signUpForm.findOne({ email: email });
+    if (userExists) {
+      const passwordMatch = await bcrypt.compare(password, userExists.password);
+
+      if (!passwordMatch) {
+        return res.status(400).json("Invalid credential");
+      } else {
+        res.json("user signed in successfully");
+      }
+    } else {
+      res.status(400).json({ error: "user not found in database. " });
+    }
+  } catch (err) {
+    console.log("catched error while login ", err);
   }
 });
 
