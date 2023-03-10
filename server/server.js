7f515fbb75f7222f86347dc9c9ed91afaad1b8f8
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-03-10 10:47:32 +0530

    changed env file

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:process.env.FRONTEND_URL,
     credentials:true,
 }));
 app.use(require("./router/auth"));
