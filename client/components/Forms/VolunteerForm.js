ed148fbafcadca8a6ed7ddb28819de03e1f160f4
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-03-11 13:01:26 +0530

    bug fixes

--- client/components/Forms/VolunteerForm.js
+++ client/components/Forms/VolunteerForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styles from "../../styles/components/forms/commonStyle.module.css";
+import styles from "../../styles/components/Forms/commonStyle.module.css";
 import axios from "axios";
 import useGeoLocation from "hooks/useGeoLocation";
 
