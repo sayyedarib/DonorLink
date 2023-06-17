48024396518a2ce0e6141b857402547382e2f2a0
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-06-17 15:27:42 +0530

    refactored and optimized code
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- server/router/vounteer/volunteerList.js
+++ server/router/vounteer/volunteerList.js
@@ -2,9 +2,8 @@ const router = require("express").Router();
 const volunteerData = require("../../models/volunteerRegistration");
 
 router.get("/", async (req, res) => {
-    const volunteers = await volunteerData.find();
-    console.log(volunteers);
-    res.json(volunteers);
+    const volunteers = await volunteerData.find({});
+    res.status(200).json(volunteers);
   });
 
 module.exports = router;
--- server/router/vounteer/volunteerRegistration.js
+++ server/router/vounteer/volunteerRegistration.js
@@ -1,18 +1,14 @@
 const router  = require("express").Router();
 const volunteerData = require("../../models/volunteerRegistration");
 
-router.post("/volunteerRegistration", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
-      console.log("i'm inside server of volunteer registration");
       const { picture, name, email, phone, bio, address, coordinates } = req.body;
-      console.log("2");
       const response = await volunteerData.findOne({ email: email });
-      console.log("3");
       if (response) {
         console.log("email already exists");
         return res.status(200).json("email already exists");
       }
-      console.log("4");
       const data = new volunteerData({
         picture,
         name,
@@ -22,11 +18,12 @@ router.post("/volunteerRegistration", async (req, res) => {
         address,
         coordinates,
       });
-      console.log("5");
       await data.save();
-      console.log("6");
+
+      res.status(200).json({message: "volunteer data saved successfully."});
     } catch (err) {
-      console.log("got an error while volunteer registration ", err);
+      console.log("got an error while volunteer registration", err);
+      res.status(500).json({message:"got an error while volunteer registration "})
     }
   });
 
