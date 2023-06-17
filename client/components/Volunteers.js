5051da58f7209e68fa238a2961046b2ef08089ba
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-06-17 15:27:06 +0530

    rafactored and optimized code
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- client/components/Volunteers.js
+++ client/components/Volunteers.js
@@ -2,35 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import VolunteerCard from "./VolunteerCard";
 import styles from "../styles/components/volunteers.module.css";
-import { BACKEND_URL } from "next.config";
 
-const Volunteers = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("start checking response");
-        const response = await axios.get(`${BACKEND_URL}/api/volunteers`);
-        console.log("response ", response);
-        const data = response.data;
-        console.log(data);
-        setData(data);
-        return data;
-      } catch (err) {
-        console.log("error while fetching volunteers data ", err);
-      }
-    };
-    fetchData();
-  }, []);
+const Volunteers = ({volunteerData}) => {
 
   return (
     <>
       <section className={styles.volunteerSection}>
         <h1>Our Volunteers</h1>
         <div>
-          {data.map((data) => {
-            console.log("volunteers data ", data);
+          {volunteerData?.map((data) => {
             return (
               <VolunteerCard
                 key={data._id}
