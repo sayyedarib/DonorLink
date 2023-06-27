6ac3b3ea299345899954520648e41edebd6214e6
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-06-27 23:05:38 +0530

    added footer and contactUs page
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,6 +4,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import UserState from "@/context/auth/UserState";
 // import Navbar from "@/components/Navigation";
 import Navigation from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 config.autoAddCss = false;
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }) {
       {/* <Navbar/> */}
       <Navigation/>
       <Component {...pageProps} />
+      <Footer />
     </UserState>
 
   );
