37cfe9079e5948b9a2e550380b41508c49c2cb81
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-06-28 10:32:57 +0530

    fixed footer to bottom and bug fixes in login server

--- server/router/auth/login.js
+++ server/router/auth/login.js
@@ -8,11 +8,11 @@ router.post("/", async (req, res) => {
         const { email, password } = req.body;
         const { loginType } = req.query;
         const responseVolunteer = await volunteerData.findOne({ email: email });
-        const responseUser = await userData.findOne({ email: email });
+        const responseDonor = await userData.findOne({ email: email });
         console.log("SR:auth-login", responseVolunteer);
 
-        if(loginType=="google"&&responseVolunteer){
-            return res.status(200).send({ userData: responseVolunteer, message: "loggedIn successfully" })
+        if(loginType=="google"&&responseVolunteer||responseDonor){
+            return res.status(200).send({ userData: responseVolunteer?responseVolunteer:responseDonor, message: "loggedIn successfully" })
         }
 
         if (responseVolunteer) {
@@ -26,21 +26,21 @@ router.post("/", async (req, res) => {
             return res.status(200).send({ userData: responseVolunteer, message: "loggedIn successfully" })
         }
 
-        else if (responseUser) {
+        else if (responseDonor) {
             const validPassword = await bcrypt.compare(
                 password,
-                responseUser.password
+                responseDonor.password
             );
             if (!validPassword) {
                 return res.status(409).send({ message: "Invalid Email or Password" });
             }
-            return res.status(200).send({ userData: responseUser, message: "loggedIn successfully" })
+            return res.status(200).send({ userData: responseDonor, message: "loggedIn successfully" })
         }
 
         else {
             return res.status(409).send({ message: "Email doesn't exits please signUp" });
         }
-        
+
     } catch (err) {
         console.log("SR: got an error while volunteer login", err);
         res.status(500).send({ message: "got an error while login" })
