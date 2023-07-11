1799781794dd27cd29759943cac0fe8dd926bb0e
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-07-11 13:01:53 +0530

    added copy button to upi id
    Signed-off-by: Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>

--- client/components/layout/Header.js
+++ client/components/layout/Header.js
@@ -1,25 +1,40 @@
 import React from "react";
 import Link from "next/link";
-import {useState} from "react";
-import {AiFillCloseCircle} from "react-icons/ai"
-
+import { useState } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { FaCopy } from "react-icons/fa";
 
 const Header = () => {
-  const [qrPopup, setQrpopup] = useState(false);
+  const [qrPopup, setQrPopup] = useState(false);
+
+  const handleCopy = () => {
+    const paymentDetails = "sayyedaribhussain4321@okhdfcbank";
+    navigator.clipboard.writeText(paymentDetails);
+  };
 
   return (
     <>
       <header className="bg-blue-50 flex flex-wrap-reverse sm:flex-nowrap px-10 lg:px-20 text-left justify-center items-center h-[85vh] content-center">
         <div className="w-auto sm:w-1/2 flex flex-col gap-2 sm:gap-6 md:gap-8">
-          <h1 className="text-4xl sm:text-5xl font-bold text-blue-700">Who we are?</h1>
+          <h1 className="text-4xl sm:text-5xl font-bold text-blue-700">
+            Who we are?
+          </h1>
           <h2 className="">
-            We are a platform of donation that bridge the gap between willing donors and needy people maintining the trust with 100% transparency. Join us as volunteer, donor
-            or peoole of need.
+            We are a platform of donation that bridge the gap between willing
+            donors and needy people maintaining the trust with 100% transparency.
+            Join us as a volunteer, donor, or people in need.
           </h2>
           <div className="flex gap-4 flex-wrap">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded" onClick={()=>setQrpopup(true)}>Support</button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+              onClick={() => setQrPopup(true)}
+            >
+              Support
+            </button>
             <Link href="/auth?register=true">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">Join Us</button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded">
+                Join Us
+              </button>
             </Link>
           </div>
         </div>
@@ -30,15 +45,24 @@ const Header = () => {
       {qrPopup && (
         <div className="fixed z-50 top-0 left-0 right-0 bottom-0 bg-black bg-opacity-70 flex items-center justify-center">
           <div className="relative p-7 bg-white">
-            <span className="absolute right-2 top-2 cursor-pointer" onClick={()=>setQrpopup(false)}><AiFillCloseCircle className="text-2xl"/></span>
+            <span
+              className="absolute right-2 top-2 cursor-pointer"
+              onClick={() => setQrPopup(false)}
+            >
+              <AiFillCloseCircle className="text-2xl" />
+            </span>
             <img src="/assets/images/img/QR.png" alt="QR" className="w-96 h-96" />
             <div className="absolute bottom-0 left-0 right-0 bg-white text-black text-center py-3">
-              
-            <span className="p-3">
-              sayyedaribhussain4321@okhdfcbank
-            </span>
-            <br/>
-            <span className="p-3">*payment gateway is under test-mode </span>
+              <span className="p-3"><b>UPI ID</b>:sayyedaribhussain4321@okhdfcbank</span>
+              <button
+                className="bg-blue-700 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded mt-3"
+                onClick={handleCopy}
+              >
+                <div className="flex items-center">
+                <FaCopy className="mr-2" />
+                <span>Copy</span>
+                </div>
+              </button>
             </div>
           </div>
         </div>
